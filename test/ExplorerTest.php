c63873c0f04af703175e2232c779a75a06c4370b
Jordi <jmdesarrollo82@gmail.com>
2024-10-10 13:19:41 +0200

    feature: made the explorer entity tests (RED)

---
+++ test/ExplorerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+use KataExplorer\Entity\Explorer;
+use PHPUnit\Framework\TestCase;
+
+class ExplorerTest extends TestCase
+{
+    private Explorer $explorer;
+
+    public function setup(): void
+    {
+        $this->explorer = new Explorer();
+    }
+
+    public function testCanInstantiate(): void
+    {
+        $this->assertInstanceOf(Explorer::class, $this->explorer);
+    }
+
+    public function testCanMoveNorth(): void
+    {
+        $this->explorer->goNorth();
+        self::assertEquals(1, $this->explorer->getPositionY());
+    }
+
+    public function testCanMoveSouth(): void
+    {
+        $this->explorer->goSouth();
+        self::assertEquals(-1, $this->explorer->getPositionY());
+    }
+
+    public function testCanMoveEast(): void
+    {
+        $this->explorer->goEast();
+        self::assertEquals(1, $this->explorer->getPositionX());
+    }
+
+    public function testCanMoveWest(): void
+    {
+        $this->explorer->goWest();
+        self::assertEquals(-1, $this->explorer->getPositionX());
+    }
+}
